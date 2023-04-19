4eb7ef176b9d5a08964690130e1c528016e99b87
DJDuck86 <danielj86@fbcor.org>
2023-04-18 18:50:22 -0700

    Fixes allowing app to merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Pokemon from './Pokemon';
 import './App.css';
-import Caretaker from './Caretaker';
 import SearchBar from './SearchBar';
+// Comment to practice pull request :)
+// Comment to practice new branch
+
 function App() {
   return (
-    <div>
-      <SearchBar/>
-    <div>
-      <Caretaker/>
-    </div>
+    <div className="App">
+      Welcome to NU's Pokemon Adoption Center 
+      <Pokemon/>
+      <p>
+        <SearchBar/>
+      </p>
     </div>
   );
 }
