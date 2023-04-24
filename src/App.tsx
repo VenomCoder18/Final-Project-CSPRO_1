9a967bd548feb95cafa93366c40e8386be4fd9a4
Riley Foster <fosterrjames18@gmail.com>
2023-04-24 12:01:56 -0700

    Committing Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Pokemon from './Pokemon';
 import './App.css';
-import SearchBar from './searchbar';
+import SearchBar from './SearchBar';
+import Caretaker from './Caretaker';
 function App() {
   return (
     <div className="App">
       Welcome to NU's Pokemon Adoption Center 
-      <Pokemon/>
       <p>
         <SearchBar/>
+      </p>
+      <p>
+        <Caretaker/>
       </p>
     </div>
   );
